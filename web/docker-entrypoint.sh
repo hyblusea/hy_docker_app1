1dca44a1ed97fa4a76007659e4f62bba7cc134c9
hyblusea <hyblusea@users.noreply.github.com>
2026-05-04 11:45:39 +0800

    Fix: add default BACKEND_URL fallback and debug output in entrypoint

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
+BACKEND_URL=${BACKEND_URL:-http://backend:8080}
+export BACKEND_URL
 envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
 exec "$@"

869e83cdee0360c0dc150b7607354d7a117a80c2
hyblusea <hyblusea@users.noreply.github.com>
2026-05-06 22:36:40 +0800

    perf: Monaco Editor加载优化，换用npmmirror国内CDN加速

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { loader } from '@monaco-editor/react'
 import App from './App'
 import './styles/theme.css'
+
+loader.config({ paths: { vs: 'https://registry.npmmirror.com/monaco-editor/0.45.0/files/min/vs' } })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
